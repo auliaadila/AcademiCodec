fd861ae03960989d2113a0c1843a5ea5f13814bc
auliaadila <auliaadila36@gmail.com>
2025-02-16 15:05:14 +0900

    Add scripts in Encodec_24k_240d

--- egs/Encodec_24k_240d/start.sh
+++ egs/Encodec_24k_240d/start.sh
@@ -1,14 +1,33 @@
+# #!/bin/bash
+# source path.sh
+# log_root=logs
+# # 24kHz *.wav in train_data_dir
+# train_data_dir=dump/train
+# valid_data_dir=dump/valid
+
+# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+# python3 -m torch.distributed.launch --nproc_per_node 8 ${BIN_DIR}/main_launch.py \
+#         --BATCH_SIZE 16 \
+#         --N_EPOCHS 300 \
+#         --save_dir ${log_root} \
+#         --PATH ${log_root} \
+#         --train_data_path ${train_data_dir} \
+#         --valid_data_path ${valid_data_dir} \
+#         --sr 24000 \
+#         --ratios 6 5 4 2 \
+#         --target_bandwidths 1 2 4 8 12
+
 #!/bin/bash
 source path.sh
-log_root=logs
+log_root=checkpoints
 # 24kHz *.wav in train_data_dir
-train_data_dir=dump/train
-valid_data_dir=dump/valid
+train_data_dir=/home/s2310401/AcademiCodec/data/train_wav
+valid_data_dir=/home/s2310401/AcademiCodec/data/dev_wav
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python3 -m torch.distributed.launch --nproc_per_node 8 ${BIN_DIR}/main_launch.py \
-        --BATCH_SIZE 16 \
-        --N_EPOCHS 300 \
+export CUDA_VISIBLE_DEVICES=0
+torchrun --nproc_per_node 1 ${BIN_DIR}/main_launch.py \
+        --BATCH_SIZE 2 \
+        --N_EPOCHS 10 \
         --save_dir ${log_root} \
         --PATH ${log_root} \
         --train_data_path ${train_data_dir} \
--- egs/Encodec_24k_240d/test.sh
+++ egs/Encodec_24k_240d/test.sh
@@ -1,10 +1,24 @@
+# #!/bin/bash
+# source path.sh
+
+# python3 ${BIN_DIR}/test.py \
+#        --input=./test_wav \
+#        --output=./output \
+#        --resume_path=checkpoint/encodec_24khz_240d.pth \
+#        --sr=24000 \
+#        --ratios 6 5 4 2 \
+#        --target_bandwidths 1 2 4 8 12 \
+#        --target_bw=12 \
+#        -r
+
+
 #!/bin/bash
 source path.sh
 
 python3 ${BIN_DIR}/test.py \
-       --input=./test_wav \
-       --output=./output \
-       --resume_path=checkpoint/encodec_24khz_240d.pth \
+       --input=/home/s2310401/AcademiCodec/data/test_wav \
+       --output=./output/ckpt-hf \
+       --resume_path=/home/s2310401/AcademiCodec/egs/Encodec_24k_240d/ckpt-hf/encodec_24khz_240d.pth \
        --sr=24000 \
        --ratios 6 5 4 2 \
        --target_bandwidths 1 2 4 8 12 \
